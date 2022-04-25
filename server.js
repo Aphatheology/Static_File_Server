9764cb3165259c518863c7850560463008be7c8d
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-04-25 22:52:05 +0100

    Did some changes in the port before. I just change it to the default

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const os = require('os');
 const path = require("path")
-const port = 3000;
+const port = 5000;
 const host = "127.0.0.1"
 
 const server = http.createServer((req, res) => {
