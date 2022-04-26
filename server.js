ac68a7f58e0452b96cf022701e9f91fcc32705dc
miles-sos <frank.ebuzoeme@gmail.com>
2022-04-26 18:28:55 +0100

    added the various routing

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const http = require('http');
 const fs = require('fs');
 const os = require('os');
 
+/* Code should be in the code block of the http server */
+const urlPath = req.url;
+
+if (urlPath === '/' || urlPath === '/home') {
+} else if (urlPath === '/about') {
+} else if (urlPath === '/sys') {
+} else {
+}
+/* The end */
+
 const port = 4000;
 const host = '127.0.0.1';
